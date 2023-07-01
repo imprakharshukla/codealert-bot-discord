219da7e7ca44cdce1c60e792e9491217e97029de
areeburrub <areeburrub@gmail.com>
2023-07-02 01:28:54 +0530

    removed devfolio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from contests.contest_data_api import fetchContestAPI
+#from contests.contest_data_api import fetchContestAPI
 from hackathon.hackathon_data_api import fetchHackathonAPI
-from hackathon.devfolio_hackathon_api import fetchDevfolioHackathonAPI
+#from hackathon.devfolio_hackathon_api import fetchDevfolioHackathonAPI
 
 # fetchContestAPI()
 fetchHackathonAPI()
+# fetchDevfolioHackathonAPI()
